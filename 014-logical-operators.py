f1f632cc8bdf54922530360d28542e2f536f8630
Juan Camilo Lopez Morales <juan.lopez@confiar.coop>
2025-05-21 11:12:41 -0500

    feat(doc): added logical operators

---
+++ 014-logical-operators.py
@@ -0,0 +1,16 @@
+# Validate conditions
+
+# And
+print(True and True) # True
+print(True and False) # False
+print(False and False) # False
+
+# Or
+print(True or True) # True
+print(True or False) # True
+print(False or False) # True
+
+# Not
+print(not True) # False
+print(not False) # True
+print(not False and True) # True
