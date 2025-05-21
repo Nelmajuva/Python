23fbadff6bd817cbba9ccda44ba412e23d367f88
Juan Camilo Lopez Morales <juan.lopez@confiar.coop>
2025-05-21 11:18:35 -0500

    feat(doc): added comparation operators

---
+++ 015-comparation-operators.py
@@ -0,0 +1,18 @@
+# Equals to
+
+print(5 == 5) # True
+print(3 == 5) # False
+print("World" == "World") # True
+print("world" == "World") # False
+
+print(5 != 5) # False
+print(3 != 5) # True
+print("World" != "World") # False
+print("world" != "World") # True
+
+# Operators
+
+print(4 > 3) # True
+print(7 < 2) # False
+print(3 >= 3) # True
+print(4 <= 4) # True
