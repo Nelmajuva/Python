d506b029b12af28001554b39428dc605862693e0
Juan Camilo Lopez Morales <juan.lopez@confiar.coop>
2025-05-21 11:02:24 -0500

    feat(doc): advance learning python

--- 011-conditionals.py
+++ 011-conditionals.py
@@ -6,3 +6,23 @@ if is_old:
     print("Es mayor.")
 else:
     print("Es menor.")
+
+# Elif Conditional
+
+is_old = True
+is_licenced = True
+
+if is_old and is_licenced:
+    print("Puede manejar.")
+elif not is_old and not is_licenced:
+    print("No puede manejar, porqué es menor de edad y no tiene licencia.")
+elif is_old and not is_licenced:
+    print("No puede manejar, porqué no tiene licencia.")
+else:
+    print("No puede manejar, porqué no tiene la mayoría de edad.")
+
+# Ternary Conditional
+
+message = "Es mayor de edad" if is_old else "No es mayor de edad"
+
+print(message)
